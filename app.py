102594f155098b80a95b5076539539bcc711a2d0
Yash Bora <yabora@cisco.com>
2017-06-19 13:26:44 -0700

    Updated format for check-in page

--- app.py
+++ app.py
@@ -177,7 +177,8 @@ def make_app():
 if __name__ == "__main__":
 	app = make_app()
 	http_server = tornado.httpserver.HTTPServer(app)
-	port = int(os.environ.get("PORT",2000))
+	port = int(os.environ.get("PORT",5000))
 	http_server.listen(port)
 	print("Running at localhost:5000")
 	tornado.ioloop.IOLoop.current().start()
+	print("running")
