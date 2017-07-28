6a679d6f9a1f541cfb93c173a3073107a6bece94
Sharabesh <sharabesh@berkeley.edu>
2017-07-27 20:33:34 -0700

    Actually fixed search functionality

--- models.py
+++ models.py
@@ -188,8 +188,8 @@ with any decent size user base
 def search(query, table, start):
 	query = query.replace(" ", "%")
 	if table == "p":
-		q = Match(Posts.content, query) | Match(Posts.author, query) | Match(Posts.title, query)
-		return Posts.select(Posts, User).join(User).where(q,Posts.anonymous == False,User.firstname.contains(query),User.lastname.contains(query)).limit(10).offset(start).naive().execute()
+		q = Match(Posts.content, query) | Match(Posts.author, query) | Match(Posts.title, query) | (User.firstname.contains(query)) |(User.lastname.contains(query))
+		return Posts.select(Posts, User).join(User).where(q,Posts.anonymous == False).limit(10).offset(start).naive().execute()
 	else:
 		q = Match(User.username, query) | \
 			Match(User.department, query) | (User.firstname.contains(query)) | \
